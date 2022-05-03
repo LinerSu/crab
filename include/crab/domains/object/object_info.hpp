f19b6b2c2672f3546fa55a28fb45fca7341305b8
Yusen Su <y256su@uwaterloo.ca>
2022-05-03 00:56:15 -0400

    feat(object): add copied info for each object

--- include/crab/domains/object/object_info.hpp
+++ include/crab/domains/object/object_info.hpp
@@ -11,18 +11,19 @@ namespace object_domain_impl {
 
 /* This class contains any non-relational information about abstract objects */
 class object_info {
-  // Simple reduced product of (SmallRange x Boolean x Boolean)
+  // Simple reduced product of (SmallRange x Boolean x Boolean x Boolean)
   using product_t =
-      basic_domain_product2<small_range,
-                            basic_domain_product2<boolean_value, boolean_value>>;
+      basic_domain_product2<small_range, basic_domain_product2<boolean_value,
+                            basic_domain_product2<boolean_value, boolean_value>>>;
 
   product_t m_product;
   object_info(product_t &&product) : m_product(std::move(product)) {}
 
 public:
   object_info() { m_product.set_to_top(); }
-  object_info(small_range count, boolean_value cache_used, boolean_value cache_dirty) {
+  object_info(small_range count, boolean_value is_copied, boolean_value cache_used, boolean_value cache_dirty) {
     refcount_val() = count;
+    iscopied_val() = is_copied;
     cacheused_val() = cache_used;
     cachedirty_val() = cache_dirty;
   }
@@ -45,13 +46,16 @@ public:
 
   // Number of references that may point to an object.
   small_range &refcount_val() { return m_product.first(); }
+  // Object is copied from another. (Used by interprecedural analysis)
+  boolean_value &iscopied_val() { return m_product.second().first(); }
   // Whether the cache domain is used.
-  boolean_value &cacheused_val() { return m_product.second().first(); }
+  boolean_value &cacheused_val() { return m_product.second().second().first(); }
   // Whether the cache domain has been updated.
-  boolean_value &cachedirty_val() { return m_product.second().second(); }
+  boolean_value &cachedirty_val() { return m_product.second().second().second(); }
   const small_range &refcount_val() const { return m_product.first(); }
-  const boolean_value &cacheused_val() const { return m_product.second().first(); }
-  const boolean_value &cachedirty_val() const { return m_product.second().second(); }
+  const boolean_value &iscopied_val() const { return m_product.second().first(); }
+  const boolean_value &cacheused_val() const { return m_product.second().second().first(); }
+  const boolean_value &cachedirty_val() const { return m_product.second().second().second(); }
 
   bool operator<=(const object_info &other) const {
     return m_product <= other.m_product;
@@ -66,6 +70,7 @@ public:
   object_info operator&&(const object_info &other) const { return object_info(m_product && other.m_product); }
   void write(crab::crab_os &o) const {
     o << "RefCount=" << refcount_val() << ","
+      << "IsCopied=" << iscopied_val() << ","
       << "CacheUsed=" << cacheused_val() << ","
       << "CacheDirty=" << cachedirty_val();
   }
